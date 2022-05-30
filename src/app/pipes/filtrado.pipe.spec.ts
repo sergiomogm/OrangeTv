e95316fc934631314074bc9c3e1fcf711579d100
Sergio Muñoz <94372061+sergiomogm@users.noreply.github.com>
2022-05-30 13:24:18 +0200

    Delete src directory

--- src/app/pipes/filtrado.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { FiltradoPipe } from './filtrado.pipe';
-
-describe('FiltradoPipe', () => {
-  it('create an instance', () => {
-    const pipe = new FiltradoPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
